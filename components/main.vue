aca01830abf1dcd489461b11f3313bff3e7dadee
TJ Koury <TJ Koury>
2016-03-12 19:29:16 -0500

    'stuff'

--- components/main.vue
+++ components/main.vue
@@ -2,9 +2,6 @@
   <div class="header">
       <a href="#remove-row" class="button remove-row">-</a>
       <a href="#add-row" class="button add-row">+</a>
-      <p>
-        <a class="github-button" href="https://github.com/hootsuite/grid" data-style="mega" data-count-href="/hootsuite/grid/stargazers" data-count-api="/repos/hootsuite/grid#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star hootsuite/grid on GitHub">GitHub</a>
-      </p>
     </div>
     <div class="grid-container">
       <ul id="grid" class="grid">
@@ -15,6 +12,117 @@
     </div>
   </div>
 </template>
+
+<script>
+
+import $ from 'jquery';
+import jqueryui from 'jquery-ui';
+import map from './map.vue';
+import g from './thirdparty/jquery.gridList.js';
+import ui from '../services/ui.vue';
+import fixtures from './fixtures.js';
+
+export default{
+  name:'main',
+  data:function(){
+    return {
+      grid:false,
+      editMode:false
+    };
+  },
+  components:[map],
+  methods:{
+    buildElements:function($gridContainer, items){
+      var item, i;
+        for (i = 0; i < items.length; i++) {
+          item = items[i];
+          var $item = $(
+            '<li>' +
+              '<div class="inner">' +
+                '<div class="controls">' +
+                  '<a href="#zoom1" class="resize" data-w="1" data-h="1">1x1</a>' +
+                  '<a href="#zoom2" class="resize" data-w="2" data-h="1">2x1</a>' +
+                  '<a href="#zoom3" class="resize" data-w="3" data-h="1">3x1</a>' +
+                  '<a href="#zoom1" class="resize" data-w="1" data-h="2">1x2</a>' +
+                  '<a href="#zoom2" class="resize" data-w="2" data-h="2">2x2</a>' +
+                '</div>' +
+                i +
+              '</div>' +
+            '</li>'
+          );
+          $item.attr({
+            'data-w': item.w,
+            'data-h': item.h,
+            'data-x': item.x,
+            'data-y': item.y
+          });
+          $gridContainer.append($item);
+        }
+    },
+  },
+  ready:function(){
+
+    var DemoGrid = {
+      currentSize: 3,
+      resize: function(size) {
+        if (size) {
+          this.currentSize = size;
+        }
+        $('#grid').gridList('resize', this.currentSize);
+      },
+      flashItems: function(items) {
+        // Hack to flash changed items visually
+        for (var i = 0; i < items.length; i++) {
+          (function($element) {
+            $element.addClass('changed')
+            setTimeout(function() {
+              $element.removeClass('changed');
+            }, 0);
+          })(items[i].$element);
+        }
+      }
+  };
+
+  $(window).resize(function() {
+    $('#grid').gridList('reflow');
+  });
+
+  $(function() {
+    this.buildElements($('#grid'), fixtures.DEMO);
+
+    $('#grid').gridList({
+      lanes: DemoGrid.currentSize,
+      widthHeightRatio: 264 / 294,
+      heightToFontSizeRatio: 0.25,
+      onChange: function(changedItems) {
+        DemoGrid.flashItems(changedItems);
+      }
+    });
+    $('#grid li .resize').click(function(e) {
+      e.preventDefault();
+      var itemElement = $(e.currentTarget).closest('li'),
+          itemWidth = $(e.currentTarget).data('w'),
+          itemHeight = $(e.currentTarget).data('h');
+
+      $('#grid').gridList('resizeItem', itemElement, {
+        w: itemWidth,
+        h: itemHeight
+      });
+    });
+    $('.add-row').click(function(e) {
+      e.preventDefault();
+      DemoGrid.resize(DemoGrid.currentSize + 1);
+    });
+    $('.remove-row').click(function(e) {
+      e.preventDefault();
+      DemoGrid.resize(Math.max(1, DemoGrid.currentSize - 1));
+    });
+  }.bind(this));
+  },
+
+};
+
+</script>
 <style>
 
   * {
@@ -146,110 +254,3 @@
     }
 
 </style>
-<script>
-
-import $ from 'jquery';
-import jqueryui from 'jquery-ui';
-import map from './map.vue';
-import g from './thirdparty/jquery.gridList.js';
-import ui from '../services/ui.vue';
-import fixtures from './fixtures.js';
-
-export default{
-  name:'main',
-  data:function(){
-    return {
-      grid:false
-    };
-  },
-  components:[map],
-  ready:function(){
-
-    var DemoGrid = {
-      currentSize: 3,
-      buildElements: function($gridContainer, items) {
-        var item, i;
-        for (i = 0; i < items.length; i++) {
-          item = items[i];
-          var $item = $(
-            '<li>' +
-              '<div class="inner">' +
-                '<div class="controls">' +
-                  '<a href="#zoom1" class="resize" data-w="1" data-h="1">1x1</a>' +
-                  '<a href="#zoom2" class="resize" data-w="2" data-h="1">2x1</a>' +
-                  '<a href="#zoom3" class="resize" data-w="3" data-h="1">3x1</a>' +
-                  '<a href="#zoom1" class="resize" data-w="1" data-h="2">1x2</a>' +
-                  '<a href="#zoom2" class="resize" data-w="2" data-h="2">2x2</a>' +
-                '</div>' +
-                i +
-              '</div>' +
-            '</li>'
-          );
-          $item.attr({
-            'data-w': item.w,
-            'data-h': item.h,
-            'data-x': item.x,
-            'data-y': item.y
-          });
-          $gridContainer.append($item);
-        }
-      },
-      resize: function(size) {
-        if (size) {
-          this.currentSize = size;
-        }
-        $('#grid').gridList('resize', this.currentSize);
-      },
-      flashItems: function(items) {
-        // Hack to flash changed items visually
-        for (var i = 0; i < items.length; i++) {
-          (function($element) {
-            $element.addClass('changed')
-            setTimeout(function() {
-              $element.removeClass('changed');
-            }, 0);
-          })(items[i].$element);
-        }
-      }
-  };
-
-  $(window).resize(function() {
-    $('#grid').gridList('reflow');
-  });
-
-  $(function() {
-    DemoGrid.buildElements($('#grid'), fixtures.DEMO);
-
-    $('#grid').gridList({
-      lanes: DemoGrid.currentSize,
-      widthHeightRatio: 264 / 294,
-      heightToFontSizeRatio: 0.25,
-      onChange: function(changedItems) {
-        DemoGrid.flashItems(changedItems);
-      }
-    });
-    $('#grid li .resize').click(function(e) {
-      e.preventDefault();
-      var itemElement = $(e.currentTarget).closest('li'),
-          itemWidth = $(e.currentTarget).data('w'),
-          itemHeight = $(e.currentTarget).data('h');
-
-      $('#grid').gridList('resizeItem', itemElement, {
-        w: itemWidth,
-        h: itemHeight
-      });
-    });
-    $('.add-row').click(function(e) {
-      e.preventDefault();
-      DemoGrid.resize(DemoGrid.currentSize + 1);
-    });
-    $('.remove-row').click(function(e) {
-      e.preventDefault();
-      DemoGrid.resize(Math.max(1, DemoGrid.currentSize - 1));
-    });
-  });
-  },
-
-};
-
-</script>
